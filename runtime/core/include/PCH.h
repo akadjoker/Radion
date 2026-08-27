ce3174f33faa582df8e9aabde8fae6b4f10db708
Luis Santos <luis.lm.santos@ctw.bmwgroup.com>
2026-08-27 07:47:30 +0100

    3  seteps

--- runtime/core/include/PCH.h
+++ runtime/core/include/PCH.h
@@ -18,7 +18,7 @@
 // Expensive, stable, and used nearly everywhere. GL and SDL stay out: only
 // gpu/backends/gl and core need them, and putting them here would spread them
 // across the whole engine.
-#include "MathCompat.h"
+#include "Math.h"
 
 #include "Log.h"
 #include "Types.h"
